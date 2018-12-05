4032e47d9dae9351a84d4cf44aa4a424fba1ee50
The Broons <davidbrown090391@gmail.com>
2018-12-05 12:05:18 -0800

    added new IP

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '52.91.247.222',
+      host: '54.92.245.192',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
